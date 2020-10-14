88678031ba67a1483444b1d7ca488dc1f772cac6
Worble <rsc93@protonmail.com>
2020-10-14 13:03:41 +0100

    Update user.js

--- firefox/user.js
+++ firefox/user.js
@@ -20,3 +20,4 @@ user_pref("browser.messaging-system.whatsNewPanel.enabled", false);
 user_pref("media.hardwaremediakeys.enabled", false);
 user_pref("full-screen-api.transition-duration.enter", "0 0");
 user_pref("full-screen-api.transition-duration.leave", "0 0");
+user_pref("browser.ctrlTab.recentlyUsedOrder", false);
